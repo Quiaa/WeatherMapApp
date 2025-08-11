bd717a6240e075a38bbc5294433c4acef54d72b2
Quiaa <aerenyavuz69@gmail.com>
2025-08-11 23:30:46 +0300

    V0.8.1 - UI Improvement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.example.weathermapapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
